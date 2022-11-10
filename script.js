9e2ef45a0d3d8cca83eed220dc8b8028c8359177
Asadov <asadov@Asadovs-MacBook-Pro.local>
2022-11-10 11:25:13 +0100

    on the way

---
+++ script.js
@@ -0,0 +1,39 @@
+const quizData = [
+    {
+        question: 'How old is Florin'
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language
+        in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Pyton',
+        d: 'Javascript',
+        correct: 'a'
+        }, {
+            question: 'Who is the President of US?',
+            a: 'Florin Pop',
+            b: 'Donald Trump',
+            c: 'Ivan Saldano',
+            d: 'Mihai Andrei',
+            correct: 'b'
+        }, {
+            question: 'What does HTML stand for?',
+            a: 'Hypertext Markup Language',
+            b: 'Cascading Style Style Sheet',
+            c: 'Jason Object',
+            d: 'Holicopters Terminas Motorboats',
+            correct: 'a'
+        }, {
+            question: 'What year was Javascript launched?',
+            a: '1996',
+            b: '1998',
+            c: '1999',
+            d: 'none of the above',
+            correct: 'd'
+        }
+]
